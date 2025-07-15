c8b611f39e55cdbe7a67b548d4bd164618847bfe
Joyce <gathiithijoyce74@gmail.com>
2025-07-15 12:42:43 +0300

    Add for_loop_number_triangle.py

---
+++ LOOPS/For_loop_number_triangle.py
@@ -0,0 +1,4 @@
+for i in range(1, 6):         # i = which row we’re on
+    for j in range(1, i + 1): # j = count from 1 to the row number
+        print(j, end=" ")     # print j with a space
+    print()                   # jump to a new line
