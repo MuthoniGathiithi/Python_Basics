70ca28769bc1709f86afa97b693ab76eca8f1db6
Joyce <gathiithijoyce74@gmail.com>
2025-07-19 20:20:32 +0300

    added in models session

--- MODELS/model_attendance.py
+++ MODELS/model_attendance.py
@@ -26,3 +26,37 @@ class Unit(models.Model):
   name=models.CharField(max_length=100)
   code=models.CharField(max_length=10)
   course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+
+
+#session models 
+
+from django.db import models
+from django.utils import timezone
+from accounts.models import Unit, Profile
+import qrcode
+from io import BytesIO
+from django.core.files.base import ContentFile
+
+class Session(models.Model):
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    lecturer = models.ForeignKey(Profile, on_delete=models.CASCADE, limit_choices_to={'role': 'lecturer'})
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    qr_code = models.ImageField(upload_to='qr_codes/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        # Auto-generate a QR code using unit code and date
+        qr_data = f"{self.unit.code}_{self.date}"
+        qr = qrcode.make(qr_data)
+        buffer = BytesIO()
+        qr.save(buffer)
+        filename = f"{self.unit.code}_{self.date}.png"
+        self.qr_code.save(filename, ContentFile(buffer.getvalue()), save=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.unit.name} - {self.date} ({self.start_time}-{self.end_time})"
+
