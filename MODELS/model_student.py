8996c426a23492171d9374ce661fc32307f2fddf
Joyce <gathiithijoyce74@gmail.com>
2025-07-19 16:20:45 +0300

    created class Unit in model_student.py

--- MODELS/model_student.py
+++ MODELS/model_student.py
@@ -1,5 +1,17 @@
 from django.db import models
 class Student(models.Model):
   name=models.CharField(max_length=100)
-  age=models.IntegerField(max_length=3)
+  age=models.IntegerField()
   school=models.CharField(max_length=100)
+
+  course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+class Course(models.Model):
+  name=models.CharField(max_length=100)
+  code=models.CharField(max_length=10)
+  time=models.TimeField()
+  
+class Unit(models.Model):
+  name=models.CharField(max_length=100)
+  code=models.CharField(max_length=10)
+  course = models.ForeignKey('Course', on_delete=models.CASCADE)
