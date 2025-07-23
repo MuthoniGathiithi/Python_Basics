5f8799aca99b6a9b9868e3b3a5a7ab16c751ebb4
Joyce <gathiithijoyce74@gmail.com>
2025-07-23 14:45:18 +0300

    Add function to convert average score into grade letter

--- FUNCTIONS/grade_analyzer.py
+++ FUNCTIONS/grade_analyzer.py
@@ -1,2 +1,13 @@
 def get_average(grades):
     return sum(grades) / len(grades)
+def grade_feedback(average):
+    if average >= 80:
+        return 'A'
+    elif average >= 70:
+        return 'B'
+    elif average >= 60:
+        return 'C'
+    elif average >= 50:
+        return 'D'
+    else:
+        return 'F'
