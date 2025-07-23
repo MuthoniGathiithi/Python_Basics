db65f887a3892b442d0f4e78b6e24d3466776734
Joyce <gathiithijoyce74@gmail.com>
2025-07-23 13:26:11 +0300

    Created AgeInFuture project to calculate age in 5, 10, and 15 years

---
+++ VARIABLES/age_in_future_tracker.py
@@ -0,0 +1,5 @@
+current_age=int(input("Enter your current age: "))
+age_in_5_years=current_age+5
+age_in_10_years=current_age+10
+age_in_15_years=current_age+15
+print(f"In 5 years, you will be {age_in_5_years} years old. \nIn 10 years, you will be {age_in_10_years} years old. \nIn 15 years, you will be {age_in_15_years} years old. ")
